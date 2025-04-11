4a88e71a7b5c0275b6eba5805ed6561dbf7d3d28
KennySo <HoyanSo1996@163.com>
2025-04-12 02:01:25 +0800

    Init.

---
+++ src/main/java/com/lps/yi/translator/Application.java
@@ -0,0 +1,18 @@
+package com.lps.yi.translator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Class Application
+ *
+ * @author KennySo
+ * @date 2025/4/12
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
